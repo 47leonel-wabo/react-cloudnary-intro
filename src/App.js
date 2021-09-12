a7b01a45b47fc2d9f088c31ff00c448b97f55eb1
leonel47 <waboleonel@gmail.com>
2021-09-12 13:50:36 -0400

    Using environment variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { useState } from 'react';
 import './App.css';
+import {Image, Transformation} from 'cloudinary-react'
 
 function App() {
 
   const [fileSelected, setFileSelected] = useState(null)
+  const [uploadedImg, setUploadedImg] = useState({})
 
   const handleChange = (event) => {
     const file = event.target.files[0]
@@ -20,9 +22,10 @@ function App() {
   const handleSubmit = (e) => {
 
     // send object to cloud using axios
-    axios.post('https://api.cloudinary.com/v1_1/stelle-style/image/upload', fileSelected)
+    axios.post(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD}/image/upload`, fileSelected)
     .then(response => {
-      console.log(response);
+      console.log(response.data);
+      setUploadedImg({...uploadedImg, secure_url: response.data.secure_url, signature: response.data.signature, public_id: response.data.public_id})
     })
     .catch(error => {console.error(error);})
   }
@@ -31,6 +34,21 @@ function App() {
     <div className="App">
       <input type='file' style={{backgroundColor: '#f5f5f5', padding: '16px', marginTop: '16px'}} onChange={handleChange} />
       <button onClick={handleSubmit}>Upload File</button>
+      <div>
+        <Image
+          cloudName={`${process.env.REACT_APP_CLOUDINARY_CLOUD}`}
+          publicId={`https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_CLOUD}/image/upload/v1631446461/vlz8yxagjp5wcgl8k12l.png`}
+        >
+          <Transformation width='200' height='200' crop='thumb'/>
+          </Image>
+      </div>
+      <div>
+        <Image
+          cloudName={`${process.env.REACT_APP_CLOUDINARY_CLOUD}`}
+          publicId={uploadedImg.public_id}
+        />
+        <span>Image signature: {uploadedImg.signature}</span>
+      </div>
     </div>
   );
 }
